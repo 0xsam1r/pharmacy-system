428b1f7c21bb98ff2dff01bd18cb54c2fcf7bfcd
masri <mahmoudelasyedahmed@gmail.com>
2025-11-06 13:08:27 +0200

    (fixed error )

--- src/model/Product/Medicine.java
+++ src/model/Product/Medicine.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Products;
+package model.Product;
 /*
 import java.util.ArrayList;
 /**
@@ -36,6 +36,7 @@ public class Medicine extends Product{
 }
 */
 import java.util.ArrayList;
+import enums.Category;
 
 public class Medicine extends Product {
 
